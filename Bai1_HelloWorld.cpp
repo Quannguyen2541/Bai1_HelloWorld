890a9e0a1c84c4fd86b18d59a8c9155dacd95f41
Quannguyen2541 <quandz.5114@gmail.com>
2024-11-04 11:23:46 +0700

    Add project files.

---
+++ Bai1_HelloWorld.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main() {
+	cout << "Hello World!\n";
+	cout << " Toi ten la " << "\tNguyen Phuoc Minh Quan " << endl;
+	cout << " Toi dang hoc " << "\tC++ " << endl;
+	return 0;
+
+}
